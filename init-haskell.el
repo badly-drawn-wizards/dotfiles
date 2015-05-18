2e0b4d7492ee7ab90ffe0ca6c3387c792fe15055
= <reuben.steenekamp@gmail.com>
2015-05-18 02:00:20 +0200

    Added haskell-mode

---
+++ init-haskell.el
@@ -0,0 +1,18 @@
+(require 'use-package)
+
+(use-package ghc
+  :config (progn
+	    (add-hook 'haskell-mode-hook 'ghc-init))
+  :ensure t)
+
+(use-package haskell-mode
+  :ensure t
+  :config (progn
+	    (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+	    (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+	    (setq haskell-process-type 'cabal-repl))
+	    (setq haskell-notify-p t)
+	    (setq haskell-tags-on-save t)
+	    (setq haskell-stylish-on-save t)
+	    (setq haskell-indentation-mode))
+
