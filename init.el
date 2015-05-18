77c0c4d308417652ce2b3905066648ae07ebf8e4
= <reuben.steenekamp@gmail.com>
2015-05-18 02:00:57 +0200

    Add init-haskell.el to init.el

--- init.el
+++ init.el
@@ -1,12 +1,12 @@
 (defun config (x) (load (concat "~/.emacs.d/" x)))
 
 (config "init-package")
-
 (config "init-server")
+(config "deinit-annoyances")
+(config "init-theme")
 (config "init-company")
 (config "init-magit")
 (config "init-evil")
 (config "init-idris")
-(config "init-theme")
+(config "init-haskell")
 (config "init-popwin")
-(config "deinit-annoyances")
