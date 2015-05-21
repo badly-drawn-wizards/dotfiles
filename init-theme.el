710570f0cd5dd874cc1345e6f146c6ecba8b552d
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2015-05-21 11:22:18 +0200

    Changed solarized-theme to moe-theme+powerline

--- init-theme.el
+++ init-theme.el
@@ -1,4 +1,13 @@
 (require 'use-package)
 (use-package solarized-theme
   :ensure t
-  :config (load-theme 'solarized-dark t))
+ ;; :config (load-theme 'solarized-dark t)
+  )
+(require 'powerline)
+(use-package moe-theme
+  :ensure t
+  :config (progn
+	    (require 'powerline)
+	    (load-theme 'moe-dark t)
+	    (powerline-moe-theme)
+	    (moe-theme-random-color)))
