bd78bbd0b5db3bfae2789eec4b5abe7e8be57abd
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2015-05-25 20:06:11 +0200

    Use haskell indentation and don't show them

--- .emacs.d/init-haskell.el
+++ .emacs.d/init-haskell.el
@@ -9,10 +9,10 @@
   :ensure t
   :config (progn
 	    (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-	    (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+	    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+	    (setq haskell-indentation-show-indentations nil)
 	    (setq haskell-process-type 'cabal-repl))
 	    (setq haskell-notify-p t)
 	    (setq haskell-tags-on-save t)
-	    (setq haskell-stylish-on-save t)
-	    (setq haskell-indentation-mode))
+	    (setq haskell-stylish-on-save t))
 
