97968e74e33c2ae6f18b8ed178c19a7d08adb513
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2015-05-31 12:50:17 +0200

    Configured scala-mode indentation

--- .emacs.d/init-scala.el
+++ .emacs.d/init-scala.el
@@ -1,6 +1,11 @@
 (require 'use-package)
 
 (use-package scala-mode2
+  :init (progn
+	  (setq scala-indent:default-run-on-strategy 1)
+	  (setq scala-indent:indent-value-expression t)
+	  (setq scala-indent:align-parameters t)
+	  (setq scala-indent:align-forms t))
   :ensure t)
 
 (use-package sbt-mode
