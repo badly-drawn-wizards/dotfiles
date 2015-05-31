ae256103bce5a11e498750e81c9f743fcdfff3b1
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2015-05-31 12:52:59 +0200

    Made fullscreen behave nicely

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,14 +1,16 @@
 import XMonad
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 
 main = do
      xmonad $ defaultConfig
        { modMask = mod4Mask
        , startupHook = startup
-       , manageHook = manageDocks <+> manageHook defaultConfig
-       , layoutHook = avoidStruts $ layoutHook defaultConfig }
+       , manageHook = manageDocks <+> (isFullscreen --> doFullFloat) <+> manageHook defaultConfig
+       , layoutHook = avoidStruts $ layoutHook defaultConfig}
 
 startup :: X ()
 startup = do
         spawn "xmobar"
         spawn "emacs --daemon"
+        
