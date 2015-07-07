81d3e22483a8be720d4345c341ce02bda3cfa066
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2015-07-07 23:07:35 +0200

    Added volume keys

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,15 +1,28 @@
 import XMonad
+import XMonad.Actions.Volume (lowerVolume, raiseVolume)
+import XMonad.Util.Dzen (dzenConfig)
+import XMonad.Util.EZConfig (additionalKeys)
+
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 
-main = do
-     xmonad $ defaultConfig
+import Graphics.X11.ExtraTypes.XF86
+
+alert :: (Show a) => a -> X ()
+alert = dzenConfig return . show
+
+main :: IO ()
+main = xmonad $ defaultConfig
        { modMask = mod4Mask
        , startupHook = startup
        , manageHook = manageDocks <+> (isFullscreen --> doFullFloat) <+> manageHook defaultConfig
        , layoutHook = avoidStruts $ layoutHook defaultConfig}
+       `additionalKeys`
+       [((0, xF86XK_AudioLowerVolume), lowerVolume 4 >>= alert . floor)
+       ,((0, xF86XK_AudioRaiseVolume), raiseVolume 4 >>= alert . floor)]
 
+startup :: X ()
 startup = do
-        spawn "xmobar"
-        spawn "emacs --daemon"
+  spawn "xmobar"
+  spawn "emacs --daemon"
         
