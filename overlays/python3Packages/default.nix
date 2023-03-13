54359454527ba42ada4c7b471b9dfe9c3c39aeb6
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2023-03-13 15:16:08 +0200

    Remove unused broken overlay for diffusers

--- overlays/python3Packages/default.nix
+++ overlays/python3Packages/default.nix
@@ -3,18 +3,6 @@ let inherit (self) callPackage; in
 {
   python3 = super.python3.override {
     packageOverrides = pself: psuper: {
-      diffusers = super.mach-nix.mkPython {
-        requirements = ''
-        diffusers
-        distlib==0.3.6
-        '';
-
-        providers = {
-          _default = "nixpkgs";
-          distlib = "nixpkgs";
-        };
-
-      };
     };
   };
 }
