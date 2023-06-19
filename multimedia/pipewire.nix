6bcc8ff316742d6f94badf2aed853e50f5265d64
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2023-06-19 10:31:29 +0200

    Remove unused pulseaudio config.

--- multimedia/pipewire.nix
+++ multimedia/pipewire.nix
@@ -2,24 +2,22 @@
 
 let
   usePipewire = true;
-  usePulseaudio = false;
 in
 {
-  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = !usePulseaudio;
+    pulse.enable = true;
 
     wireplumber.enable = true;
   };
 
-  hardware.pulseaudio = lib.mkIf usePulseaudio {
-    enable = true;
-    extraModules = [ pkgs.pulseaudio-modules-bt ];
-    package = pkgs.pulseaudioFull;
-    support32Bit = true;
-  };
+  # hardware.pulseaudio = lib.mkIf usePulseaudio {
+  #   enable = true;
+  #   extraModules = [ pkgs.pulseaudio-modules-bt ];
+  #   package = pkgs.pulseaudioFull;
+  #   support32Bit = true;
+  # };
 
 }
