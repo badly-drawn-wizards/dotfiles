bda3fd8454a3a0fee6b877241bbfbc661dee1431
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2023-07-21 05:45:17 +0200

    Add flite to tts.

--- home-manager/tts.nix
+++ home-manager/tts.nix
@@ -10,7 +10,13 @@ let
     stripRoot=false;
     sha256 = "sha256-BbAoryDFYD95DK7PactjeQkhLTbhEmnHfbZJRaZ2EBk=";
   };
-  tts-packages = with pkgs; [mimic piper-tts espeak-ng coreutils sox];
+  mimicEnv = ".local/share/mimic/.env";
+  mimic-dispatch = pkgs.writeScriptBin "mimic-dispatch" ''
+    #!${pkgs.bash}/bin/bash
+    source ${config.home.homeDirectory}/${mimicEnv}
+    tee /tmp/last-mimic-dispatch | ${pkgs.docker}/bin/docker exec -i mimic3 run-venv mimic3 --remote --interactive --voice "$VOICE" --length-scale "$SCALE" --play-program "pw-play"
+  '';
+  tts-packages = with pkgs; [mimic-dispatch flite espeak-ng coreutils sox];
 in
 {
     systemd.user.services.speech-dispatcher = {
@@ -39,29 +45,32 @@ in
         LogLevel 3
         LogDir /tmp
         # AddModule "piper" "sd_generic" "piper.conf"
-        AddModule "mimic" "sd_generic" "mimic.conf"
-        AddModule "espeak-ng" "sd_espeak-ng" "espeak-ng.conf"
-        AddModule "dummy" "sd_dummy" ""
+        # AddModule "mimic" "sd_generic" "mimic.conf"
+        # AddModule "espeak-ng" "sd_espeak-ng" "espeak-ng.conf"
+        # AddModule "dummy" "sd_dummy" ""
+        AddModule "flite" "sd_flite" ""
 
-        DefaultVoiceType "MALE1"
-        DefaultModule "mimic"
         DefaultLanguage "en"
+        DefaultVoiceType "MALE1"
+        DefaultModule "flite"
         AudioOutputMethod "libao"
       '';
 
       ".local/share/piper/models/ryan".source = ryan-model;
 
-      "${moduleDir}/piper.conf".text = ''
-        GenericRateAdd 1.6
-        GenericRateMultiply 0.01
-        GenericExecuteSynth "printf %s \'$DATA\' | ${pkgs.piper-tts}/bin/piper -m ${ryan-model}/en-us-ryan-high.onnx -f - | ${pkgs.sox}/bin/play - tempo \"$RATE\""
-        AddVoice "en" "MALE1" "piper"
+      ${mimicEnv}.text = ''
+        export VOICE="en_UK/apope_low"
+        export SCALE="0.6"
       '';
+
       "${moduleDir}/mimic.conf".text = ''
-        GenericRateAdd 1.6
-        GenericRateMultiply 0.01
-        GenericExecuteSynth "printf %s \'$DATA\' | ${pkgs.docker}/bin/docker exec -it run-venv mimic3 --remote --voice \'$VOICE\' --stdout | ${pkgs.sox}/bin/play - tempo \"$RATE\""
+        # GenericRate is not working
+        GenericRateAdd 150
+        GenericRateMultiply 1
+        GenericExecuteSynth "printf %s \'$DATA\' | mimic-dispatch"
         AddVoice "en" "MALE1" "mimic"
+      '';
+      "${moduleDir}/flite.conf".text = ''
       '';
     };
 
