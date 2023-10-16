27bc4ffa06c38734cb739c095bc6f7a9f4a061a2
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2023-10-16 11:26:57 +0200

    Stuff

--- networking/default.nix
+++ networking/default.nix
@@ -4,10 +4,20 @@
   networking = {
     hostName = "noobnoob";
     networkmanager.enable = true;
+    hosts =
+    {
+      "127.0.0.1" = [
+        # "reddit.com"
+        # "www.reddit.com"
+        # "news.ycombinator.com"
+        # "youtube.com"
+        # "www.youtube.com"
+      ];
+    };
 
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 3000 ];
+      checkReversePath = false;
     };
   };
 }
