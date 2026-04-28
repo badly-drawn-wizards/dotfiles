e16896cf9ff6d90b6ec3925d91c4213fe3c0522a
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2026-04-28 19:51:29 +0200

    use dotnet 10 and set DOTNET_ROOT

--- home-manager/lang/dotnet.nix
+++ home-manager/lang/dotnet.nix
@@ -1,7 +1,13 @@
 { config, lib, pkgs, ... }:
+let
+  dotnet-sdk = pkgs.dotnet-sdk_10;
+in
 {
-  home.packages = with pkgs; [
-    # dotnet-sdk_11
-    dotnet-sdk_10
+  home.packages = [
+    dotnet-sdk
   ];
+
+  home.sessionVariables = {
+    "DOTNET_ROOT" = "${dotnet-sdk}/share/dotnet";
+  };
 }
